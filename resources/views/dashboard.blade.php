81863f7ae5e52a7810c627accb61dfa759401f84
mohammad <mohamadrobatjazi1387@gmail.com>
2022-07-10 22:19:51 +0430

    Add external libraries

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>داشبورد🤗</title>
+    <link rel="icon" href="../images/logo/logo.png">
+    <!-- css -->
+    <link rel="stylesheet" href="../css/all.css">
+    <!-- js -->
+    <script src="../js/all.js"></script>
+    <!-- font -->
+    <link rel="stylesheet" href="../fonts/font/primary-aviny.css">
+    <!-- font - fontawesome -->
+    <link rel="stylesheet" href="../fonts/fontawesome-5.6.3/css/all.min.css">
+</head>
+
+<body>
+محمد مهدی رباط جزی
+</body>
+
+</html>
---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../images/logo/logo.png">
+    <title>Dashboard .ToDo🤗</title>
+    <link rel="stylesheet" href="../fonts/font/primary-aviny.css">
+    <link rel="stylesheet" href="../css/app.css">
+</head>
+
+<body>
+    
+</body>
+
+</html>
